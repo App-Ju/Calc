34f90dcba3e007164519fc644745cdfd1dc69925
AppJu <appjuice@mail.ru>
2021-11-10 09:58:56 +0700

    added button number

--- src/App.vue
+++ src/App.vue
@@ -1,36 +1,38 @@
 <template>
   <div class="app">
     <label>
-      <input type="number">
+      <input
+          v-model='inputValue'
+          type="number">
     </label>
     <div id="keyboard">
       <div class="line">
-        <button data-ac="true">AC</button>
-        <button data-change="true">+/-</button>
-        <button data-percent="true">%</button>
-        <button data-divide="true">/</button>
+        <button >AC</button>
+        <button >+/-</button>
+        <button >%</button>
+        <button >/</button>
       </div>
       <div class="line">
-        <button data-num="true">7</button>
-        <button data-num="true">8</button>
-        <button data-num="true">9</button>
-        <button data-multiply="true">*</button>
+        <button @click="addNumber">7</button>
+        <button @click="addNumber">8</button>
+        <button @click="addNumber">9</button>
+        <button >*</button>
       </div>
       <div class="line">
-        <button data-num="true">4</button>
-        <button data-num="true">5</button>
-        <button data-num="true">6</button>
-        <button data-minus="true">-</button>
+        <button @click="addNumber">4</button>
+        <button @click="addNumber">5</button>
+        <button @click="addNumber">6</button>
+        <button >-</button>
       </div>
       <div class="line">
-        <button data-num="true">1</button>
-        <button data-num="true">2</button>
-        <button data-num="true">3</button>
-        <button data-plus="true">+</button>
+        <button @click="addNumber">1</button>
+        <button @click="addNumber">2</button>
+        <button @click="addNumber">3</button>
+        <button >+</button>
       </div>
       <div class="line">
         <button data-del="true">&lt;</button>
-        <button data-num="true">0</button>
+        <button @click="addNumber">0</button>
         <button data-comma="true">,</button>
         <button data-equals="true">=</button>
       </div>
@@ -44,11 +46,13 @@ export default {
   name: 'App',
   data () {
     return {
-
+      inputValue: ''
     }
   },
   methods: {
-
+    addNumber (event) {
+      this.inputValue += event.target.textContent
+    }
   }
 }
 </script>
